d9c96f19269bf6eb10b7381a223825c432bb430d
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-07-11 21:22:29 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -46,10 +46,10 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 int main() {
     int value =0;
   //  value=batteryIsOk(46,81,0.9);
-    value=batteryIsOk(44,79,0.7);
-    printf("value=%d",value);
-    assert(value);
-   //assert(batteryIsOk(BOUNDARY_VAL_TEMP2, BOUNDARY_VAL_SOC2, BOUNDARY_VAL_CHARGE_RATE2)==0);
+  //  value=batteryIsOk(44,79,0.7);
+   // printf("value=%d",value);
+    //assert(value);
+   assert(batteryIsOk(BOUNDARY_VAL_TEMP2, BOUNDARY_VAL_SOC2, BOUNDARY_VAL_CHARGE_RATE2)==0);
    //assert(batteryIsOk(BOUNDARY_VAL_TEMP3, BOUNDARY_VAL_SOC3, BOUNDARY_VAL_CHARGE_RATE3)==0);
    return TRUE;
 }
