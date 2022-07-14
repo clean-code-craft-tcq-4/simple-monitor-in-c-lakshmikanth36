6ee3e3421740ab747ebb5733d1cd4147bc81f724
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-07-14 21:36:56 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -40,5 +40,14 @@ int main() {
      assert(!batteryIsOk(500,81,0.9));
      assert(!batteryIsOk(1,19,0.7));
      assert(batteryIsOk(25,21,0.7)==0);
+     assert(batteryIsOk(25,22,0.7)==0);
+     assert(batteryIsOk(25,23,0.7)==0);
+     assert(batteryIsOk(25,24,0.7)==0);
+     assert(batteryIsOk(25,25,0.7)==0);
+     assert(batteryIsOk(25,76,0.7)==0);
+     assert(batteryIsOk(25,77,0.7)==0);
+     assert(batteryIsOk(25,78,0.7)==0);
+     assert(batteryIsOk(25,79,0.7)==0);
+     assert(batteryIsOk(25,80,0.7)==0);
  return 0;
 }
