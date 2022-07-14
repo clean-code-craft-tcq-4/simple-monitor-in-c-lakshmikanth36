04616e00cfc6555e88d189125ae897b28458c47b
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-07-14 20:24:56 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -3,50 +3,18 @@
 float readchargeRate(float chargeRate)
 {
     if(chargeRate > CHARGE_RATE_MAX)
-    {
         printf("Charge Rate out of range!\n");
-        return TRUE;
-    }
-    else
-        return FALSE;
 }
 
-float readsoc(float soc)
+float readtemperatureorsoc(float temperatureorsoc,int min_val,int max_val)
 {
-    if(soc < SOC_MIN || soc > SOC_MAX)
-    {
-        printf("State of Charge out of range!\n");
-        return TRUE;
-    }
-    else
-        return FALSE;
-}
+    if((temperatureorsoc < min_val) || (temperatureorsoc > max_val))
+        printf("Temperature/State of Charge out of range!\n");
+ }
 
-float readtemperature(float temperature)
-{
-    if((temperature < TEMP_MIN) || (temperature > TEMP_MAX))
-    {
-        printf("Temperature out of range!\n");
-        return TRUE;
-    }
-    else
-        return FALSE;
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    int retval=0;
-    retval=readtemperature(temperature);
-    retval=readsoc(soc);
-    retval=readchargeRate(chargeRate);
-    if(retval!=0)
-        return TRUE;
-    else
-        return FALSE;
-}
-
-int main() {
-   assert(batteryIsOk(BOUNDARY_VAL_TEMP2, BOUNDARY_VAL_SOC2, BOUNDARY_VAL_CHARGE_RATE2));
-   assert(batteryIsOk(44,79,0.7)==0);
-   assert(batteryIsOk(25,70,0.6)==0);
-   return TRUE;
+int batteryIsOk(float temperature, float soc,float chargeRate) {
+    readtemperatureorsoc(temperature,TEMP_MIN,TEMP_MAX);
+    readtemperatureorsoc(soc,SOC_MIN,SOC_MAX);
+    readchargeRate(chargeRate);
+return 0;
 }
