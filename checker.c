3af0423d77dad0f74fa0a00c4cb7a3c512b9af8a
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-07-14 20:31:24 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -18,3 +18,16 @@ int batteryIsOk(float temperature, float soc,float chargeRate) {
     readchargeRate(chargeRate);
 return 0;
 }
+int main() {
+     assert(batteryIsOk(0,20,0.0)==0);
+     assert(batteryIsOk(5,25,0.1)==0);
+     assert(batteryIsOk(25,50,0.4)==0);
+     assert(batteryIsOk(40,75,0.7)==0);
+     assert(batteryIsOk(45,80,0.8)==1);
+     assert(batteryIsOk(50,85,0.9)==1);
+     assert(batteryIsOk(100,81,0.9)==1);
+     assert(batteryIsOk(400,81,0.9)==1);
+     assert(batteryIsOk(500,81,0.9)==1);
+     assert(batteryIsOk(1,19,0.7)==1);
+ return 0;
+}
