b88bde5b96f4f1b88b68ea86b7fceb488e7d8193
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-07-15 20:57:03 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -18,6 +18,7 @@ float ConvertToCelcius(float farenheit)
 }
 
 int batteryIsOk(float temperature, float soc,float chargeRate) {
+    int conversion_temperature  = 0;
     conversion_temperature = ConvertToCelcius(temperature);
     readtemperatureorsoc(conversion_temperature,TEMP_MIN,TEMP_MAX);
     readtemperatureorsoc(soc,SOC_MIN,SOC_MAX);
