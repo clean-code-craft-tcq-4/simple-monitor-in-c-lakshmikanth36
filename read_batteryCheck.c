a6e5dc4bd3e718dd0b4090f5b547faa5847f36b6
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-07-15 21:02:07 +0530

    Create read_batteryCheck.c

---
+++ read_batteryCheck.c
@@ -0,0 +1,25 @@
+#include "checker.h"
+
+void readchargeRate(float chargeRate)
+{
+    if(chargeRate > CHARGE_RATE_MAX) 
+        printf("Charge Rate out of range!\n"); 
+}
+
+void readtemperatureorsoc(float temperatureorsoc,int min_val,int max_val)
+{
+    if((temperatureorsoc < min_val) || (temperatureorsoc > max_val))
+        printf("Temperature/State of Charge out of range!\n");
+ }
+
+float ConvertToCelcius(float farenheit)
+{
+    return (farenheit - 32) * 5 / 9;
+}
+void warningalert_SOC(int temperatureorsoc)
+{
+    if(temperatureorsoc <=SOC_MIN+4)  
+        printf("Approaching discharge \n");
+    else if(temperatureorsoc >= SOC_MAX-4)
+        printf("Approaching charge-peak \n");
+}
